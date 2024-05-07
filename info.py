06f9db6deadd2c47d58fac111ac2d87325d824e8
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-05-07 07:49:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,34 +18,11 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 PORT = os.environ.get("PORT", "8080")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-REQUESTED_CHANNEL = int(os.environ.get("REQUESTED_CHANNEL", ""))
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
-ADMIN_CHANNEL_ID = int(os.environ.get("ADMIN_CHANNEL_ID", ""))
-EVAL_ID = int(os.environ.get("EVAL_ID", ""))
-
-# clone bots
-LOG_CHANNEL_INFORM = int(os.environ.get("LOG_CHANNEL_INFORM", ""))
-LOG_CHANNEL_ERROR = int(os.environ.get("LOG_CHANNEL_ERROR", ""))
-
-# important information for your bot
-S_GROUP = environ.get('S_GROUP', "https://t.me/sdbots_support")
-S_CHANNEL = environ.get('S_CHANNEL', "https://t.me/sd_bots")
-
-#sample
-#F_SUB = os.environ.get("FORCE_SUB", "sd_bots") 
-F_SUB = os.environ.get("FORCE_SUB", "") 
-
 
 # for mongodb
 DATABASE_NAME = os.environ.get("DB_NAME", "")     
 DATABASE_URI  = os.environ.get("DB_URL", "")
 MONGO_URL = os.environ.get('MONGO_URL', "")
 
-#for spotify 
-SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID', 'd3a0f15a75014999945b5628dca40d0a')
-SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET', 'e39d1705e35c47e6a0baf50ff3bb587f')
-
-#for google
-G_API_KEY = os.environ.get('G_API_KEY','AIzaSyAGv5kIu2-E0N9eTdK7lzevl2nr3sOk6is')
 
 
