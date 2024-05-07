8868ca041d71aa6d72e67a3672f2d8e434eefe09
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-07 07:41:26 +0530

    Create stats.py

---
+++ plugins/stats.py
@@ -0,0 +1,61 @@
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
+from info import ADMINS
+from database.users_db import db
+from datetime import date, datetime 
+from utils import get_size, temp, extract_user, get_file_id,humanbytes, last_online
+from Script import script
+import re
+from pyrogram.errors import ChatAdminRequired
+import asyncio
+
+@Client.on_message(filters.command('users') & filters.user(ADMINS))
+async def list_users(bot, message):
+    # https://t.me/GetTGLink/4184
+    raju = await message.reply('Getting List Of Users')
+    users = await db.get_all_users()
+    out = "Users Saved In DB Are:\n\n"
+    async for user in users:
+        out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
+        if user['ban_status']['is_banned']:
+            out += '( Banned User )'
+        out += '\n'
+    try:
+        await raju.edit_text(out)
+    except MessageTooLong:
+        with open('users.txt', 'w+') as outfile:
+            outfile.write(out)
+        await message.reply_document('users.txt', caption="List Of Users")
+
+@Client.on_message(filters.command('chats') & filters.user(ADMINS))
+async def list_chats(bot, message):
+    raju = await message.reply('Getting List Of chats')
+    chats = await db.get_all_chats()
+    out = "Chats Saved In DB Are:\n\n"
+    async for chat in chats:
+        out += f"**Title:** `{chat['title']}`\n**- ID:** `{chat['id']}`"
+        if chat['chat_status']['is_disabled']:
+            out += '( Disabled Chat )'
+        out += '\n'
+    try:
+        await raju.edit_text(out)
+    except MessageTooLong:
+        with open('chats.txt', 'w+') as outfile:
+            outfile.write(out)
+        await message.reply_document('chats.txt', caption="List Of Chats")
+
+
+@Client.on_message(filters.command('stats'))
+async def get_stats(bot, message):
+ try:
+    rju = await message.reply('Fetching stats..')
+    total_users = await db.total_users_count()
+    totl_chats = await db.total_chat_count()
+    size = await db.get_db_size()
+    free = 536870912 - size
+    size = get_size(size)
+    free = get_size(free)
+    await rju.edit(script.STATUS_TXT.format(total_users, totl_chats, size, free))
+ except Exception as e:
+    await messaag.reply_text(e)
