e77f313991b7d43f2923ccd8a31fda6e5c006c59
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-05-10 11:49:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,16 +12,16 @@ def is_enabled(value, default):
     elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
     else: return default
 
-API_ID = int(os.environ.get('API_ID', ''))
-API_HASH = os.environ.get('API_HASH', '')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+API_ID = int(os.environ.get('API_ID', '8914119'))
+API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '7135774957:AAGcLDtGkseQVcoaN5xxBel0I5Ht15kQWWk')
 PORT = os.environ.get("PORT", "8080")
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1342641151')]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002055184812'))
 
 # for mongodb
-DATABASE_NAME = os.environ.get("DB_NAME", "")     
-DATABASE_URI  = os.environ.get("DB_URL", "")
+DATABASE_NAME = os.environ.get("DB_NAME", "cBSSRccyjoHQMuAT")     
+DATABASE_URI  = os.environ.get("DB_URL", "mongodb+srv://o53317853:cBSSRccyjoHQMuAT@cluster0.aerrmcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MONGO_URL = os.environ.get('MONGO_URL', "")
 
 
