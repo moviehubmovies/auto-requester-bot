a5670d12793302ecc55e2ae5213cfa44873ba9b0
moviehubmovies <169069384+moviehubmovies@users.noreply.github.com>
2024-05-23 20:56:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,10 +14,10 @@ def is_enabled(value, default):
 
 API_ID = int(os.environ.get('API_ID', '7679954'))
 API_HASH = os.environ.get('API_HASH', '4af51af8f1a8b06ca2b076370ba93fba')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '7038259688:AAEutmQz_-fr_MGu6zPXoxbFUeabKpLUVSM')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '1447478080:AAESvxi-7JjMSkWsGv2WdHLMX-u7lwgBp1k')
 PORT = os.environ.get("PORT", "8080")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2144812475')]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001760331496'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002239864052'))
 
 # for mongodb
 DATABASE_NAME = os.environ.get("DB_NAME", "newbot")     
