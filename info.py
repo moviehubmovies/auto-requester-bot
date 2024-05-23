7b74fdbc8b960072041e43c24984a678cf674211
ALNWALKERTG <166110788+ALNWALKERTG@users.noreply.github.com>
2024-05-23 14:28:54 +0530

    NEW VARIBLES

--- info.py
+++ info.py
@@ -12,16 +12,16 @@ def is_enabled(value, default):
     elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]: return False
     else: return default
 
-API_ID = int(os.environ.get('API_ID', ''))
-API_HASH = os.environ.get('API_HASH', '')
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+API_ID = int(os.environ.get('API_ID', '7679954'))
+API_HASH = os.environ.get('API_HASH', '4af51af8f1a8b06ca2b076370ba93fba')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '7038259688:AAEutmQz_-fr_MGu6zPXoxbFUeabKpLUVSM')
 PORT = os.environ.get("PORT", "8080")
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '')]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2144812475')]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001760331496'))
 
 # for mongodb
-DATABASE_NAME = os.environ.get("DB_NAME", "")     
-DATABASE_URI  = os.environ.get("DB_URL", "")
+DATABASE_NAME = os.environ.get("DB_NAME", "newbot")     
+DATABASE_URI  = os.environ.get("DB_URL", "mongodb+srv://newbot:newbot@cluster0.hybkhvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MONGO_URL = os.environ.get('MONGO_URL', "")
 
 
